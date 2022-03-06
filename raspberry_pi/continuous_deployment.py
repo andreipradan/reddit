b6a9428ee82c293d350acd327a70c44a88bb3d27
Andrei Prădan <andrei.pradan@gmail.com>
2022-03-06 14:05:30 +0200

    moved show-logs to shell script

--- raspberry_pi/continuous_deployment.py
+++ raspberry_pi/continuous_deployment.py
@@ -13,21 +13,3 @@ def setup():
     run_cmd("sudo ./raspberry_pi/set-services.sh", silent=False)
     set_url()
     logger.info("Completed setup, showing logs. CTRL+C to terminate")
-    show_logs()
-
-
-def show_logs():
-    cmd = "journalctl --follow " \
-          "_SYSTEMD_UNIT=fastapi.service + " \
-          "_SYSTEMD_UNIT=ngrok.service + " \
-          "_SYSTEMD_UNIT=reddit.service"
-    logger.info(f"Running {cmd}")
-    process = subprocess.Popen(cmd.split(" "), stdout=subprocess.PIPE)
-    while True:
-        output = process.stdout.readline()
-        if output == "" and process.poll() is not None:
-            break
-        if output:
-            print(output.strip())
-    rc = process.poll()
-    return rc
---
+++ raspberry_pi/show-logs.sh
@@ -0,0 +1,1 @@
+journalctl --follow _SYSTEMD_UNIT=fastapi.service + _SYSTEMD_UNIT=ngrok.service + _SYSTEMD_UNIT=reddit.service

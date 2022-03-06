430020cf352d13a3742fa53ca031160d1286f5e7
Andrei Prădan <andrei.pradan@gmail.com>
2022-03-07 00:36:13 +0200

    updated scripts

--- raspberry_pi/cd.py
+++ raspberry_pi/cd.py
@@ -6,20 +6,31 @@ from reddit.settings import LOGGING_FORMAT
 
 logging.basicConfig(format=LOGGING_FORMAT)
 logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
 
 
 def cd_setup():
     permissions = "pi ALL=(ALL) NOPASSWD: /usr/bin/systemctl restart reddit.service"
-    logger.info(run_cmd(f'echo "{permissions}" >> /etc/sudoers.d/pi', silent=False))
-    logger.info(run_cmd("sudo ./raspberry_pi/copy-services.sh", silent=False))
+    output = run_cmd(f'echo "{permissions}" >> /etc/sudoers.d/pi', silent=False)
+    if not output:
+        raise ValueError("Could not copy permissions to sudoers.d/pi")
+    logger.info(output)
+    output = run_cmd("sudo ./raspberry_pi/scripts/copy-services.sh", silent=False)
+    if not output:
+        raise ValueError("Could not copy services to systemd")
+    logger.info(output)
     start_services()
     github_hook.set_url()
     logger.info("Completed setup, showing logs. CTRL+C to terminate")
+    show_logs()
 
 
 def start_services():
-    logger.info(run_cmd("sudo ./raspberry_pi/start-services.sh", silent=False))
+    output = run_cmd("sudo ./raspberry_pi/scripts/start-services.sh", silent=False)
+    if not output:
+        raise ValueError("Could not start services")
+    logger.info(output)
 
 
 def show_logs():
-    logger.info(run_cmd("./raspberry_pi/show-logs.sh", silent=False))
+    run_cmd("./raspberry_pi/scripts/show-logs.sh", silent=False)

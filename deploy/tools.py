8f71bd58206c6f65d2ba1b2dcd4088b067d614a2
Andrei Prădan <andrei.pradan@gmail.com>
2022-03-09 15:00:05 +0200

    translate key

--- deploy/tools.py
+++ deploy/tools.py
@@ -50,7 +50,7 @@ def get_ngrok_url():
 
 
 def start_services():
-    run_cmd("sudo systemctl restart fastapi.service ngrok.service reddit.service")
+    run_cmd("sudo systemctl restart fastapi ngrok reddit")
 
 
 def set_url():

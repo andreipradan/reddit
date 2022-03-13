659a3a761882af1bf6fa8a406631148072ccd278
Andrei Pradan <andrei.pradan@gmail.com>
2022-03-13 21:09:00 +0200

    added sentry

--- reddit/web/main.py
+++ reddit/web/main.py
@@ -1,11 +1,14 @@
 import logging
 
+import dotenv
+import sentry_sdk
 import uvicorn
 from fastapi import FastAPI
 
 from reddit.settings import LOGGING_FORMAT
 from reddit.web import github_hook, telegram_hook
 
+sentry_sdk.init(dotenv.dotenv_values()["SENTRY_URL"], traces_sample_rate=1.0)
 app = FastAPI()
 
 logging.basicConfig(format=LOGGING_FORMAT)
